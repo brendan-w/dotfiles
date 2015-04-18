c0ad5194b2ae64f6b52979a43fb76f7cceff98a1
Brendan Whitfield <brendanw@windworksdesign.com>
2015-04-17 20:51:01 -0400

    added dotfiles for conky, fish, and terminator

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+
+alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+alias ll='ls -l --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+alias la='ls -la --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
+alias grep='grep --color=tty -d skip'
+
+
+alias gl="git log"
+alias gs="git status"
+alias gd="git diff"
+alias gb="git branch"
+#alias gt="git log --graph --simplify-by-decoration --pretty=format:'%d' --all"
+alias gt="tig"
+alias gap="git add -p"
+alias gaa="git add --all"
+alias psg="ps aux | grep"
