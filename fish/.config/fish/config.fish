5aec4f7ef32aaee7b95d2f6e5d9143c09963b53c
Brendan Whitfield <brendanw@windworksdesign.com>
2015-05-25 20:34:40 -0400

    remove psg alias in favor of pgrep, added line numbers to grep, and list all remote branches

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,15 +2,14 @@
 alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias ll='ls -l --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias la='ls -la --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
-alias grep='grep --color=tty -d skip'
+alias grep='grep -n --color=tty -d skip'
 
 
 alias gl="git log"
 alias gs="git status"
 alias gd="git diff"
 alias gds="git diff --stat"
-alias gb="git branch"
+alias gb="git branch -a"
 alias gt="git log --graph --all --pretty=format:'%C(bold yellow)%h%C(reset)%C(bold red)%d%C(reset)%n%C(white)%s%C(reset)%C(dim white) - %an%C(reset)%n'"
 alias gap="git add -p"
 alias gaa="git add --all ; git status"
-alias psg="ps aux | grep"
