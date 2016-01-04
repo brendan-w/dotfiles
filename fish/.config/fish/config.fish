a1e906b950f6a78eb43921662b3da9e0fdea8e25
Brendan Whitfield <brendanw@windworksdesign.com>
2016-01-03 19:04:06 -0500

    added !! and rename detection to gds

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,7 @@
 set fish_greeting ""
 
 # --------------- general ---------------
+alias !!=" eval $history[1]" # leading space prevents command from showing up in history
 alias ls='ls --human-readable --group-directories-first --color=auto --classify --time-style=+"%d-%m-%Y %H:%M   "'
 alias grep='grep -n --color=tty -d skip'
 alias pgrep='pgrep -a'
@@ -18,7 +19,7 @@ alias xl='atool -l'
 alias gl="git log"
 alias gs="git status"
 alias gd="git diff"
-alias gds="git diff --stat"
+alias gds="git diff --stat --find-renames"
 alias gb="git branch -a"
 alias gt="git log --graph --all --pretty=format:'%C(bold yellow)%h%C(reset)%C(bold red)%d%C(reset)%n%C(white)%s%C(reset)%C(dim white) - %an%C(reset)%n'"
 alias gap="git add -p"
