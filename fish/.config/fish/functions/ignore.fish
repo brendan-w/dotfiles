80d24518ed9ef38fc95263fb5e37f9dbf5820c37
Brendan Whitfield <brendanw@windworksdesign.com>
2016-01-15 22:17:44 -0500

    scrape language names from gitignore.io

--- fish/.config/fish/functions/ignore.fish
+++ fish/.config/fish/functions/ignore.fish
@@ -1,3 +1,10 @@
 function ignore --description 'creates .gitignore files'
-	curl --silent "https://www.gitignore.io/api/"(echo $argv | tr -s ' ' ',') > .gitignore
+
+    # if no arguments, pull a listing of languages
+    if test (count $argv) -eq 0
+        curl --silent "https://www.gitignore.io/dropdown/templates.json" | grep --only-matching "\"text\":\"[a-zA-Z]*\"" | cut -d\" --field 4 | less
+        return
+    end
+
+    curl --silent "https://www.gitignore.io/api/"(echo $argv | tr -s ' ' ',') >> .gitignore
 end
