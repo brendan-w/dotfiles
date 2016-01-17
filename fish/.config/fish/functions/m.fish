737121135e1c5fd805e67f599b87962055fd1f77
Brendan Whitfield <brendanw@windworksdesign.com>
2016-01-17 18:27:37 -0500

    only export locally. still can't get them working

--- fish/.config/fish/functions/m.fish
+++ fish/.config/fish/functions/m.fish
@@ -1,12 +1,12 @@
 function m --description 'wrapper for man'
 	# colored man pages
-    set -x LESS_TERMCAP_mb (set_color -o blue)             # begin blinking
-    set -x LESS_TERMCAP_md (set_color -o blue)             # begin bold
-    set -x LESS_TERMCAP_me (set_color normal)              # end mode
-    set -x LESS_TERMCAP_se (set_color normal)              # end standout-mode
-    set -x LESS_TERMCAP_so (set_color -b white -o black)   # begin standout-mode - info box
-    set -x LESS_TERMCAP_ue (set_color normal)              # end underline
-    set -x LESS_TERMCAP_us (set_color -o blue)             # begin underline
+    set -xl LESS_TERMCAP_mb (set_color -o blue)             # begin blinking
+    set -xl LESS_TERMCAP_md (set_color -o blue)             # begin bold
+    set -xl LESS_TERMCAP_me (set_color normal)              # end mode
+    set -xl LESS_TERMCAP_se (set_color normal)              # end standout-mode
+    set -xl LESS_TERMCAP_so (set_color -b white -o black)   # begin standout-mode - info box
+    set -xl LESS_TERMCAP_ue (set_color normal)              # end underline
+    set -xl LESS_TERMCAP_us (set_color -o blue)             # begin underline
 
     man $argv ; or apropos $argv
 end
