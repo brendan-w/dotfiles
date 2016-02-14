863e151b51075abbeb1a8acf3aeb9d85885ad5ad
Brendan Whitfield <brendanw@windworksdesign.com>
2016-02-14 01:23:11 -0500

    expand .gitignore files when doing mkproj

--- fish/.config/fish/functions/mkproj.fish
+++ fish/.config/fish/functions/mkproj.fish
@@ -8,10 +8,16 @@ function mkproj --description 'copies the requested template files from ~/dotfil
         return
     end
 
-	if [ -d "$TEMPLATES/$argv" ]
-		cp -r "$TEMPLATES/$argv/*" ./
-	else
-		echo "'$argv' is not a template in ~/dotfiles/.templates"
-	end
+    if test -d "$TEMPLATES/$argv"
+        cp -r $TEMPLATES/$argv/* $TEMPLATES/$argv/.* ./
 
+        # if there's a gitignore, use ignore() to expand it
+        if test -e .gitignore
+            set -l langs (cat .gitignore)
+            wipe .gitignore # wipe the fake gitignore
+            ignore $langs # download the appropriate ignore rules
+        end
+    else
+        echo "'$argv' is not a template in ~/dotfiles/.templates"
+    end
 end
