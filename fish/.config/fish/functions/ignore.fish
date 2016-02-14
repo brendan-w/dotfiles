c7548599ac238042476b95fc0b72e57f1a19246b
Brendan Whitfield <brendanw@windworksdesign.com>
2016-02-14 00:58:37 -0500

    use the ID field instead

--- fish/.config/fish/functions/ignore.fish
+++ fish/.config/fish/functions/ignore.fish
@@ -3,7 +3,7 @@ function ignore --description 'creates .gitignore files'
     # if no arguments, pull a listing of languages
     if test (count $argv) -eq 0
         set url "https://www.gitignore.io/dropdown/templates.json"
-        curl --silent $url | grep --only-matching "\"text\":\"[a-zA-Z]*\"" | cut -d\" --field 4 | sort | eval $PAGER
+        curl --silent $url | grep --only-matching "\"id\":\"[a-zA-Z0-9+#]*\"" | cut -d\" --field 4 | sort | less
         return
     end
 
