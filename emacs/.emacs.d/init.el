1c167f6ed0b5d4a502e3d164f13bee7eefb05a9a
Brendan Whitfield <brendanw@windworksdesign.com>
2016-03-18 22:45:36 -0400

    using helm, wired meta-arrows to change buffer focus

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,8 +5,23 @@
 (require 'init-elpa)
 
 ; load packages
+
 (use-package monokai-theme
-    :ensure t)
+    :ensure t
+    :init
+    (load-theme 'monokai t))
+
+(use-package helm
+  :ensure t
+  :init
+  (helm-mode 1)
+  :config
+  (global-set-key (kbd "M-x") 'helm-M-x))
 
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(tool-bar-mode -1) ;; hide the gui toolbar
+
+;; use M-<arrow> to change focus between visible buffers
+(require 'windmove)
+(windmove-default-keybindings 'meta)
