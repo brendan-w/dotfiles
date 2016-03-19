75d71b1af88479b78e765b8ead9f38b49da46378
Brendan Whitfield <brendanw@windworksdesign.com>
2016-03-19 18:47:22 -0400

    rearranged, added undo and window splitting

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,13 +1,34 @@
 
-; disable the startup message
-(setq inhibit-startup-message t)
+;; ---------------- builtin stuff----------------
 
-; look in ~/.emacs.d/lisp for custom lisp modules
+(setq inhibit-startup-message t) ;; disable the startup message
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(tool-bar-mode -1) ;; hide the gui toolbar
+(global-linum-mode) ;; display line numbers
+(global-hl-line-mode) ;; highlight the current line
+(blink-cursor-mode 0) ;; DON'T blink the cursor
+
+(require 'windmove) ;; use M-<arrow> to change focus between visible buffers
+(windmove-default-keybindings 'meta)
+
+(global-set-key (kbd "C-z") 'undo) ; act like every other program. also, don't run (suspend-frame)
+
+;; TODO: move window focus in the correct direction
+(global-set-key (kbd "<C-M-up>") 'split-window-vertically)
+(global-set-key (kbd "<C-M-down>") 'split-window-vertically)
+(global-set-key (kbd "<C-M-left>") 'split-window-horizontally)
+(global-set-key (kbd "<C-M-right>") 'split-window-horizontally)
+
+
+;; ---------------- packaged stuff----------------
+
+;; look in ~/.emacs.d/lisp for custom lisp modules
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
 (require 'init-elpa)
 
-; load packages
+;; load packages
 
 (use-package monokai-theme
     :ensure t
@@ -36,14 +57,4 @@
     (global-set-key (kbd "C->") 'mc/mark-next-like-this)
     (global-set-key (kbd "C-<") 'mc/mark-previous-like-this))
 
-(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-(tool-bar-mode -1) ;; hide the gui toolbar
 
-;; use M-<arrow> to change focus between visible buffers
-(require 'windmove)
-(windmove-default-keybindings 'meta)
-
-(global-linum-mode) ;; display line numbers
-(global-hl-line-mode) ;; highlight the current line
-(blink-cursor-mode 0) ;; DON'T blink the cursor
