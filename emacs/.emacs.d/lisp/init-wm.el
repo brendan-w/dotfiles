c4b86839d64ab22e01dc056d5f84a400a4a61e2d
Brendan Whitfield <brendanw@windworksdesign.com>
2016-03-19 20:28:50 -0400

    bring the cursor to the right window when splitting

--- emacs/.emacs.d/lisp/init-wm.el
+++ emacs/.emacs.d/lisp/init-wm.el
@@ -4,12 +4,28 @@
 (require 'windmove) ;; use M-<arrow> to change focus between visible buffers
 (windmove-default-keybindings 'meta)
 
-;; TODO: move window focus in the correct direction
+;; new windows are added on the lower/right,
+;; so these directions leave the cursor in the correct spot
 (global-set-key (kbd "<C-M-up>") 'split-window-vertically)
-(global-set-key (kbd "<C-M-down>") 'split-window-vertically)
 (global-set-key (kbd "<C-M-left>") 'split-window-horizontally)
-(global-set-key (kbd "<C-M-right>") 'split-window-horizontally)
 
+(defun my-split-window-vertically ()
+    (interactive)
+    (split-window-vertically)
+    (windmove-down)
+)
+
+(defun my-split-window-horizontally ()
+    (interactive)
+    (split-window-horizontally)
+    (windmove-right)
+)
+
+;; these, however, need to move the cursor down or right
+(global-set-key (kbd "<C-M-down>") 'my-split-window-vertically)
+(global-set-key (kbd "<C-M-right>") 'my-split-window-horizontally)
+
+;; window deletion
 (global-set-key (kbd "<C-M-delete>") 'delete-window)
 
 (provide 'init-wm)
