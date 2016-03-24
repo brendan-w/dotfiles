0cd597f990d7f9734fbd55c9aa5369cf119b52d7
Brendan Whitfield <brendanw@windworksdesign.com>
2016-03-24 01:06:56 -0400

    kindof ridiculous that emacs can't do this natively...

--- emacs/.emacs.d/lisp/init-utils.el
+++ emacs/.emacs.d/lisp/init-utils.el
@@ -13,4 +13,30 @@
 
 (global-set-key (kbd "C-l") 'my-select-line)
 
+
+;; make the backspace key delete whitespace to the next tab stop
+
+(defvar my-offset 4 "My indentation offset. ")
+
+(defun backspace-whitespace-to-tab-stop ()
+  "Delete whitespace backwards to the next tab-stop, otherwise delete one character."
+  (interactive)
+  (if (or indent-tabs-mode
+          (region-active-p)
+          (save-excursion
+            (> (point) (progn (back-to-indentation)
+                              (point)))))
+      (call-interactively 'backward-delete-char-untabify)
+    (let ((movement (% (current-column) my-offset))
+          (p (point)))
+      (when (= movement 0) (setq movement my-offset))
+      ;; Account for edge case near beginning of buffer
+      (setq movement (min (- p 1) movement))
+      (save-match-data
+        (if (string-match "[^\t ]*\\([\t ]+\\)$" (buffer-substring-no-properties (- p movement) p))
+            (backward-delete-char (- (match-end 1) (match-beginning 1)))
+          (call-interactively 'backward-delete-char))))))
+
+(global-set-key (kbd "<backspace>") 'backspace-whitespace-to-tab-stop)
+
 (provide 'init-utils)
