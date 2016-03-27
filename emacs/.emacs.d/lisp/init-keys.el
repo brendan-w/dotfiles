17aa3eec41118843d35856cf610a1b32e7b8717e
Brendan Whitfield <brendanw@windworksdesign.com>
2016-03-27 18:06:44 -0400

    mapped copy/paste to the usual keys

--- emacs/.emacs.d/lisp/init-keys.el
+++ emacs/.emacs.d/lisp/init-keys.el
@@ -18,6 +18,9 @@
     (define-key map (kbd "TAB")        'tab-to-tab-stop) ;; force the tab key to always indent. Don't try to be smart
     (define-key map (kbd "<backspace>") 'backspace-whitespace-to-tab-stop)
     (define-key map (kbd "C-l")        'my-select-line)
+    (define-key map (kbd "C-c C-c")    'kill-ring-save) ; copy
+    (define-key map (kbd "C-v")        'yank) ; paste
+
 
     ; (define-key map (kbd "<C-home>")   'recenter-top-bottom) ;; moves things to center/top/bottom of screen
     (define-key map (kbd "<C-home>")   'beginning-of-buffer)
