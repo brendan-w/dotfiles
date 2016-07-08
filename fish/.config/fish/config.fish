63c72625aa3ba909e73bdc9a405181f5e33e12ae
Brendan Whitfield <brendanw@windworksdesign.com>
2016-07-08 14:24:31 -0400

    aliased pushd/popd

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -20,6 +20,8 @@ alias xx='atool -x'
 alias xl='atool -l'
 alias rm='rm -r' # probably a bad idea, but we'll see
 alias cd='cds'
+alias s='pushd .' # save cwd onto the stack
+alias p='popd' # pop cwd off the stack
 
 # --------------- git ---------------
 alias gl="git log"
