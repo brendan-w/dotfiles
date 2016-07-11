bf16cb536d61e0230d608ad3b14291391df94a5a
Brendan Whitfield <brendanw@windworksdesign.com>
2016-07-11 14:45:48 -0400

    added package-initialize

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,10 @@
 
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 ;; look in ~/.emacs.d/lisp for custom lisp modules
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
